577cc874ce68941134c3a6dac083a4b8763e9201
DanielPFranca <123706802+DanielPFranca@users.noreply.github.com>
2023-08-07 13:26:40 -0300

    Delete DESM-Daniel directory

--- DESM-Daniel/Exemplo2.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:io';
-
-void main()
-{
-  print('Digite seu nome');
-
-  String nome = stdin.readLineSync()!; // Faz com que o usuário digite algo | -> ! é um operador not
-  print('Ola $nome'); // $ retorna o que tem na variável
-}
--- DESM-Daniel/Exemplo7.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:io';
-
-void main()
-{
-  for (int i = 0; i<11; i++)
-  {
-    print(i);
-  }
-}
